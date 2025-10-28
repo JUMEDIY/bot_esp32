840111ceeaf649a63ef7705d51c5ddb19f4eff9e
jumejume1 <jumejume1@hotmail.com>
2025-10-28 21:22:18 +0700

    Add files via upload

---
+++ linorobot2_bringup/launch/bringup.launch.py
@@ -0,0 +1,95 @@
+# Copyright (c) 2021 Juan Miguel Jimeno
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, PythonExpression
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch.conditions import IfCondition, UnlessCondition
+
+
+def generate_launch_description():
+
+       
+    description_launch_path = PathJoinSubstitution(
+        [FindPackageShare('linorobot2_description'), 'launch', 'description.launch.py']
+    )
+
+    ekf_config_path = PathJoinSubstitution(
+        [FindPackageShare("linorobot2_base"), "config", "ekf.yaml"]
+    )
+
+    default_robot_launch_path = PathJoinSubstitution(
+        [FindPackageShare('linorobot2_bringup'), 'launch', 'default_robot.launch.py']
+    )
+
+
+    return LaunchDescription([
+
+        DeclareLaunchArgument(
+            name='micro_ros_transport',
+            default_value='udp4',
+            description='micro-ROS transport'
+        ),
+
+        DeclareLaunchArgument(
+            name='odom_topic', 
+            default_value='/odom',
+            description='EKF out odometry topic'
+        ),
+        
+     
+        DeclareLaunchArgument(
+            name='micro_ros_transport',
+            default_value='serial',
+            description='micro-ROS transport'
+        ),
+
+        DeclareLaunchArgument(
+            name='micro_ros_port',
+            default_value='8888',
+            description='micro-ROS udp/tcp port number'
+        ),
+
+        
+        Node(
+            package='robot_localization',
+            executable='ekf_node',
+            name='ekf_filter_node',
+            output='screen',
+            parameters=[
+                ekf_config_path
+            ],
+            remappings=[("odometry/filtered", LaunchConfiguration("odom_topic"))]
+        ),
+        
+        
+
+        Node(
+           
+            package='micro_ros_agent',
+            executable='micro_ros_agent',
+            name='micro_ros_agent',
+            output='screen',
+            arguments=[LaunchConfiguration('micro_ros_transport'), '--port', LaunchConfiguration('micro_ros_port')]
+        ),
+    
+       IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(description_launch_path)
+        ),
+
+        
+    ])
